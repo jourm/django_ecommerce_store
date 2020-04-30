574768b0b7e5353b63419ce711dd28d6708c0fcd
jourm <jourm91@gmail.com>
2020-04-30 14:48:05 +0000

    Add checkout cashing in payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,16 +1,34 @@
 from django.http import HttpResponse
 
 
-class StripeWh_Handler:
-    """ Handle Stripe Webhook """
+class StripeWH_Handler:
+    """Handle Stripe webhooks"""
 
     def __init__(self, request):
         self.request = request
 
-    def handle_event(self, request):
+    def handle_event(self, event):
         """
         Handle a generic/unknown/unexpected webhook event
         """
         return HttpResponse(
-            content=f'Webhook Recived: {event["type"]}',
+            content=f'Unhandled webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+        Handle the payment_intent.succeeded webhook from Stripe
+        """
+        intent = event.data.object
+        print(intent)
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+        Handle the payment_intent.payment_failed webhook from Stripe
+        """
+        return HttpResponse(
+            content=f'Webhook received: {event["type"]}',
             status=200)
