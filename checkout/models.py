88fe17c0fc8b07e70590b7e7ce4c6b7e039b7ced
jourm <jourm91@gmail.com>
2020-04-30 12:11:46 +0000

    add order summary to checkout success page

--- checkout/models.py
+++ checkout/models.py
@@ -30,12 +30,12 @@ class Order(models.Model):
         """
         return uuid.uuid4().hex.upper()
 
-    def update_total(self):
+    def uppdate_total(self):
         """
         Update grand total each time a line item is added,
         accounting for delivery costs.
         """
-        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum']
+        self.order_total = self.lineitems.aggregate(Sum('lineitem_total'))['lineitem_total__sum'] or 0
         if self.order_total < settings.FREE_DELIVERY_THRESHOLD:
             self.delivery_cost = self.order_total * settings.STANDARD_DELIVERY_PERCENTAGE / 100
         else:
